741635db2359c3182cf3b01f1e1bce88c33a54dc
Jean-Mat <jean-mat@iMac-de-Jean-Mathias.local>
2015-06-23 22:26:57 +0200

    Corrections

--- Arduino/twitter/twitter.ino
+++ Arduino/twitter/twitter.ino
@@ -2,33 +2,32 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
-#define BRUMISATEUR  A0
-#define LEDR A1
-#define LEDG A2
-#define LEDB A3
-#define VENTILO A4
+#define BRUMISATEUR A0
+#define RUBAN_LED   A1
+#define VENTILO     A2
+#define LED_OK      A3
+#define LED_KO      A4
+#define LED_ACTION  A5
 
 //-- Adresse MAC du schild Ethernet --
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x9C, 0x03 };
-//-- On va utliser un serveur DHCP qui va nous attribuer une adresse IP automatique --
+//-- On va utliser un serveur DHCP qui va nous attribuer une adresse IP automatiquement --
 //-- Mais au cas où il ne peut pas, on affecte nous meme l'IP --
 IPAddress ip(192, 168, 0, 177);
 
 //-- Serveur à joindre pour récupérer le nombre de Tweets --
-char server[] = "home.gourdet.net";
+char server[] = "www.domaine.com";
 
 //-- On initialise le client HTTP --
 EthernetClient client;
 
 //-- Variables globales --
-const unsigned long requestInterval = 20000;  //Interval entre 2 relevé de tweets
-boolean requested;                   // whether you've made a request since connecting
+const unsigned long requestInterval = 5000;  //Interval entre 2 relevé de tweets
 unsigned long lastAttemptTime = 0;   // last time you connected to the server, in milliseconds
 
 String currentLine = "";            // string to hold the text from server
 String tweet = "";                  // string to hold the tweet
 boolean readingTweet = false;       // if you're currently reading the tweet
-
 
 
 //-- Fonction SETUP executé qu'une fois au demarrage --
@@ -40,16 +39,20 @@ void setup() {
   }
   
   //-- Configuration des pins --
+  pinMode(LED_OK, OUTPUT);
+  pinMode(LED_KO, OUTPUT);
+  pinMode(LED_ACTION, OUTPUT);
+  
   pinMode(BRUMISATEUR, OUTPUT);
-  pinMode(LEDR, OUTPUT);
-  pinMode(LEDG, OUTPUT);
-  pinMode(LEDB, OUTPUT);
+  pinMode(RUBAN_LED, OUTPUT);
   pinMode(VENTILO, OUTPUT);
     
+  digitalWrite(LED_OK, LOW);
+  digitalWrite(LED_KO, LOW);
+  digitalWrite(LED_ACTION, LOW);
+  
   digitalWrite(BRUMISATEUR, LOW);
-  digitalWrite(LEDR, LOW);
-  digitalWrite(LEDG, LOW);
-  digitalWrite(LEDB, LOW);
+  digitalWrite(RUBAN_LED, LOW);
   digitalWrite(VENTILO, LOW);
 
   //-- On demarre la connexion Ethernet uniquement avec l'adresse MAC --
@@ -75,7 +78,9 @@ void setup() {
 //-- Programme principal / boucle infinie --
 void loop() {
   if (client.connected()) {
+    
     if (client.available()) {
+      
       //-- On lit le caractère qui arrivent --
       char inChar = client.read();
       //Serial.print(inChar);
@@ -89,9 +94,11 @@ void loop() {
         currentLine = "";
       } 
       
+      //Serial.println(currentLine);
+      
       // if the current line ends with <text>, it will
       // be followed by the tweet:
-      if ( currentLine.endsWith("<nb>")) {
+      if ( currentLine.endsWith("<nbTweet>")) {
         // tweet is beginning. Clear the tweet string:
         readingTweet = true; 
         tweet = "";
@@ -114,15 +121,17 @@ void loop() {
           // close the connection to the server:
           client.stop();
           if(nbTweet > 0) { 
-            action();
+            for(int i = 0 ; i < nbTweet ; i++) {
+              action();
+              delay(1000);
+            }
           }
         }
       }
+      
     }   
   }
   else if (millis() - lastAttemptTime > requestInterval) {
-    // if you're not connected, and two minutes have passed since
-    // your last connection, then attempt to connect again:
     connectToServer();
   }
 }
@@ -132,53 +141,56 @@ void connectToServer() {
   // attempt to connect, and wait a millisecond:
   Serial.println("Connexion au serveur...");
   if (client.connect(server, 80)) {
+    cnx(true);
     Serial.println("On construit la requete HTTP...");
     // make HTTP GET request to twitter:
-    client.println("GET /zBis/index.php HTTP/1.1");
-    client.println("HOST: home.gourdet.net");
+    client.println("GET /getTweet.php HTTP/1.1");
+    client.println("HOST: www.domaine.com");
     client.println("Connexion: close");
     client.println();
   } else {
     Serial.println("Echec de connexion");
+    cnx(false);
+    
   }
   // note the time of this connect attempt:
   lastAttemptTime = millis();
 }
 
 void action() {
-  Serial.println("action");
+    Serial.println("action");
+    digitalWrite(LED_ACTION, HIGH);
   
     //-- On allume le brumisateur --
     digitalWrite(BRUMISATEUR, HIGH);
     
-    //-- 1 seconde de led rouge --
-    digitalWrite(LEDR, HIGH);
-    delay(1000);
-    digitalWrite(LEDR, LOW);
+    //-- On allume le ruban led --
+    digitalWrite(RUBAN_LED, HIGH);
     
-    //-- 1 seconde de led verte --
-    digitalWrite(LEDG, HIGH);
-    delay(1000);
-    digitalWrite(LEDG, LOW);    
-
-    //-- 1 seconde de led bleu --
-    digitalWrite(LEDB, HIGH);
-    delay(1000);
+    //-- On laisse la magie operee 10 secondes --
+    delay(10000);
     
-    //-- 5 secondes toutes les couleurs --
-    digitalWrite(LEDR, HIGH);
-    digitalWrite(LEDG, HIGH);
-    digitalWrite(LEDB, HIGH);
-    delay(8000);
-    
-    //-- On etaint tout --
+    //-- On eteint le brumisateur --
     digitalWrite(BRUMISATEUR, LOW);
-    digitalWrite(LEDR, LOW);
-    digitalWrite(LEDG, LOW);
-    digitalWrite(LEDB, LOW);
     
     //-- Un petit coups de ventilo --
     digitalWrite(VENTILO, HIGH);
-    delay(500);
+    delay(1000);
     digitalWrite(VENTILO, LOW);
+        
+    
+    //-- On eteint le ruban led --
+    digitalWrite(RUBAN_LED, LOW);
+      
+    digitalWrite(LED_ACTION, LOW);
+}
+
+void cnx(bool ok) {
+  if(ok) {
+    digitalWrite(LED_OK, HIGH);
+    digitalWrite(LED_KO, LOW);
+  } else {
+    digitalWrite(LED_OK, LOW);
+    digitalWrite(LED_KO, HIGH);
+  }
 }
